bbfe9e35f4b32842c2a6dfaae7c8c2593e4bae8b
mazu36 <65269375+mazu36@users.noreply.github.com>
2025-08-22 21:36:35 +0200

    Remove undefined variable

--- hello.py
+++ hello.py
@@ -4,7 +4,6 @@ import click
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
-    print(var)
     if name == "Thor":
         click.echo("Thor, you are always red.")
         click.echo(click.style(f"Hello {name}!", fg="red"))
