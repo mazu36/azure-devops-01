f7dc941b693573239a8b62a2cb3c5e98aa4bd9d9
mazu36 <zumaitrois@gmail.com>
2025-08-22 13:20:04 +0200

    add lint in make

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ import click
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
+    print(var)
     if name == "Thor":
         click.echo("Thor, you are always red.")
         click.echo(click.style(f"Hello {name}!", fg="red"))
